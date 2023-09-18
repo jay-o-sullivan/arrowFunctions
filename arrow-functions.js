36f158f32c260e7caf5891554267e9cc8f2e033b
jay-o-sullivan <>
2023-09-18 02:51:31 +0000

    Add arrow functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,41 @@
 
 // Vanilla JavaScript Function
 
+// function addTwoNumbers(a, b){
+//     // code block
+//     return a + b;
+// }
+// let sum = addTwoNumbers(3, 5);
+// console.log(sum)
+
 // Arrow Function With Parameters
 
+const addTwoNumbers = (a, b) => {
+    // code block
+    return a + b;
+ }
+let sum = addTwoNumbers(3, 5);
+console.log(sum)
+
 // Single Line Arrow Function With Parameters
+// const addTwoNumbers2 = (a, b) => (a + b); also valid
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers2(6, 4);
+console.log(sum2);
 
 // Implicit Returns
 
+const saysomething = message => console.log(message);
+saysomething('Hello, There!!');
+
+const sayHello = () => console.log('Hello');
+sayHello();
+
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `<p>
+        This is a multiline string
+    </p>`
+)
+console.log(returnMultipleLines());
